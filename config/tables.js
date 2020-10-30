667a5fe1bbb403dbfa2b9f094f33d5d51a5e3c1c
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 13:41:22 +0100

    Add new table item_subscriptions
    For subscribing to individual trade drop items

--- config/tables.js
+++ config/tables.js
@@ -1,8 +1,14 @@
 const serviceSubscriptions = 'service_subscriptions';
+const itemSubscriptions = 'item_subscriptions';
 
 module.exports = {
     serviceSubscriptions,
     serviceSubscription: serviceSubscriptions,
     serviceSubs: serviceSubscriptions,
     serviceSub: serviceSubscriptions,
+
+    itemSubscriptions,
+    itemSub: itemSubscriptions,
+    item: itemSubscriptions,
+    items: itemSubscriptions
 }
