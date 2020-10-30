20b8998627a8d280e913845c06606ad0cd421aaf
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 14:49:33 +0100

    Refactor: Add extra fields and reformat returned fields from foundation api

--- components/resources/foundation.js
+++ components/resources/foundation.js
@@ -7,24 +7,29 @@ module.exports = {
         let startTime = new Date().setMonth(now.getMonth() - 4);
         startTime = parseInt(startTime/1000);
         console.log('Start Time', startTime);
+
         const query = `{
             xykmarkets(where: {goLiveDate_gt: ${ startTime } }) {
                 name
                 goLiveDate
                 totalBuyPrice
+                brand {
+                    name 
+                }
             }
         }`;
 
         return axios.post(url, { query })
-        //return axios.post(url, JSON.stringify(query))
         .then(res => {
             const drops = res.data.data.xykmarkets;
             return drops.map( item => {
                 return {
-                    price: (item.totalBuyPrice / 1000000000000000000).toFixed(2),
+                    price: '$' + (item.totalBuyPrice / 1000000000000000000).toFixed(2),
                     name: item.name,
                     date: new Date(item.goLiveDate * 1000),
-                    service: 'foundation'
+                    service: 'foundation',
+                    brand: item.brand.name,
+                    img: null
                 }
             });
         });
--- tests/integration.tests/foundation.test.js
+++ tests/integration.tests/foundation.test.js
@@ -3,14 +3,16 @@ const expect = chai.expect;
 const foundation = require('../../components/resources/foundation');
 
 describe('Fetch drops from foundation', function() {
-    it('#dev Fetch drops from foundation', function() {
+    it('Fetch drops from foundation', function() {
         return foundation.fetchDrops()
         .then(res => {
             console.log('DROPS', res);
             let price0 = res[0].price;
             expect(price0).to.be.a('string');
+            expect(price0[0]).to.equal('$');
             expect(price0.length - price0.indexOf('.')).to.equal(3);
-            expect(res).to.all.have.keys('date', 'name', 'price', 'service');
+            expect(res[0]).to.have.property('service', 'foundation');
+            expect(res).to.all.have.keys('date', 'name', 'price', 'service', 'brand', 'img');
         });
     });
 });
