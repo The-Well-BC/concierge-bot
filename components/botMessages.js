266d93c56a4a5ac44c1475d80cd852e318bc8f32
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 20:21:42 +0100

    Add extra information to greeting displayed on /start command

--- components/botMessages.js
+++ components/botMessages.js
@@ -12,6 +12,10 @@ const start = function(payload) {
     else
         text += ".";
 
+    text += `
+            I'm here to alert you on products, artwork released by varying artists.
+            Right now, you can choose to subscribe to all new releases, or drops. Eventually, you will have artists you look forward to and then you can subscribe to those artists.
+            Choose 'Subscribe' to learn more about the different services you could subscribe to.`;
     return {
         text,
         reply_markup: replyMarkup
--- tests/unit.tests/messages.test.js
+++ tests/unit.tests/messages.test.js
@@ -3,7 +3,7 @@ const clone = require('rfdc')();
 const botMessages = require('../../components/botMessages');
 const samplePayload = require('../samplePayload');
 
-describe('Send messages', function() {
+describe('Start Command', function() {
     it('Reply to start command when first name is present', function() {
         let message = botMessages.prepareResponse(samplePayload.commands.start);
         expect(message).to.be.an('object');
@@ -23,6 +23,17 @@ describe('Send messages', function() {
         expect(message.text).to.have.string('Hello littlezigy.');
     });
 
+    it('Introductory message to the start command', function() {
+        let message = botMessages.prepareResponse(samplePayload.commands.start);
+        expect(message).to.be.an('object');
+        expect(message.text).to.equal(`Hello Adesuwa.
+            I'm here to alert you on products, artwork released by varying artists.
+            Right now, you can choose to subscribe to all new releases, or drops. Eventually, you will have artists you look forward to and then you can subscribe to those artists.
+            Choose 'Subscribe' to learn more about the different services you could subscribe to.`)
+    });
+});
+
+describe('The Subscribe Command', function() {
     it('Respond to /subscribe command', function() {
         const payload = clone(samplePayload.commands.subscribe);
 
