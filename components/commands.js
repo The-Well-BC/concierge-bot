0b7baed73cbfc30eb35af579dbef9db1447a54e7
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 13:51:28 +0100

    Add subscribe function for /subscribe message

--- components/commands.js
+++ components/commands.js
@@ -1,5 +1,44 @@
+const subscribedao = require('./subscription.dao');
+
+subscribe = function(command, chat_id) {
+    let services = ['foundation', 'zora'];
+    let counter = 0;
+    const subscribeAll = () => {
+        if(counter === services.length)
+            return
+        else {
+            return subscribedao.addServiceSubscription(chat_id, services[counter])
+            .then(() => {
+                counter++;
+                subscribeAll()
+            });
+        }
+    };
+
+    if(command == '/subscribe')
+        return subscribeAll();
+    else {
+        let subservice;
+        services.forEach(o => {
+            if(command.includes(o)) {
+                subservice = o;
+            }
+        });
+
+        if(subservice) {
+            return subscribedao.addServiceSubscription(chat_id, subservice)
+            .then(res => true );
+        } else return new Promise(resolve => resolve(false));
+    }
+}
+
 module.exports = {
-    runCommand: function() {
-        return new Promise(resolve => resolve(true));
+    runCommand: function(payload) {
+        let command = payload.message.text;
+
+        if(command.indexOf('/subscribe') == 0)
+            return subscribe(command, payload.message.chat.id);
+        else
+            return new Promise(resolve => resolve(true));
     }
 }
