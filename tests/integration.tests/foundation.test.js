68ff5bb378634d46d2080aa4bccbd032aae38754
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 13:45:47 +0100

    Add: Test condition that checks decimal places of trading drop prices

--- tests/integration.tests/foundation.test.js
+++ tests/integration.tests/foundation.test.js
@@ -3,8 +3,14 @@ const expect = chai.expect;
 const foundation = require('../../components/resources/foundation');
 
 describe('Fetch drops from foundation', function() {
-    return foundation.fetchDrops()
-    .then(res => {
-        expect(res).to.all.have.keys('id', 'date');
+    it('#dev Fetch drops from foundation', function() {
+        return foundation.fetchDrops()
+        .then(res => {
+            console.log('DROPS', res);
+            let price0 = res[0].price;
+            expect(price0).to.be.a('string');
+            expect(price0.length - price0.indexOf('.')).to.equal(3);
+            expect(res).to.all.have.keys('date', 'name', 'price', 'service');
+        });
     });
 });
