44342f80808ff28a86d86b6b0fcbd92f76680bdc
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 20:20:27 +0100

    Add test for alert photo message

--- tests/unit.tests/alerts.test.js
+++ tests/unit.tests/alerts.test.js
@@ -3,8 +3,8 @@ const chai = require('chai');
 chai.use( require('chai-things') );
 const expect = chai.expect;
 
-describe('Send Alerts', function() {
-    it('Returns alert object', function() {
+describe('Returns Alerts Message Object', function() {
+    it('When no image is supplied', function() {
         const chatids = [93892, 1345241, 45252];
         const resourcePayload = {
             price: '$73.58',
@@ -18,6 +18,26 @@ describe('Send Alerts', function() {
         let alertmessage = alerts.alertMessage(chatids, resourcePayload);
 
         expect(alertmessage).to.all.have.keys('chat_id', 'text');
+        expect(alertmessage).to.all.not.have.key('photo');
+        expect(alertmessage).to.include.something.with.property('chat_id', chatids[0]);
+        expect(alertmessage[0]).to.have.property('text', 'Unit—002 dropped on August 31, 2020.\nIt is currently trading at $73.58');
+    });
+
+    it('When image is supplied', function() {
+        const chatids = [93892, 1345241, 45252];
+        const resourcePayload = {
+            price: '$73.58',
+            name: 'Unit—002',
+            date: '2020-08-31T19:00:00.000Z',
+            service: 'foundation',
+            brand: 'postdigital',
+            img: 'https://boon.com/boon.jpg'
+        };
+
+        let alertmessage = alerts.alertMessage(chatids, resourcePayload);
+
+        expect(alertmessage).to.all.have.keys('chat_id', 'text', 'photo');
+        expect(alertmessage).to.all.have.property('photo', 'https://boon.com/boon.jpg');
         expect(alertmessage).to.include.something.with.property('chat_id', chatids[0]);
         expect(alertmessage[0]).to.have.property('text', 'Unit—002 dropped on August 31, 2020.\nIt is currently trading at $73.58');
     });
