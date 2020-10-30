04fd5f16c65e83ae57eaf04055ad15bad03b67ca
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-30 14:50:05 +0100

    Refactor: Changed alert string. Made it prettier

--- components/alerts.js
+++ components/alerts.js
@@ -1,6 +1,10 @@
 module.exports = {
     alertMessage(chatIDs, payload) {
-        let text = `${ payload.name } dropped on ${ payload.date }. It is currently trading at $${ payload.price }`;
+        let date = new Date(payload.date);
+        let months = 'January February March April May June July August September October November December';
+
+        let dateStr = (months.split(' '))[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+        let text = `${ payload.name } dropped on ${ dateStr }.\nIt is currently trading at ${ payload.price }`;
         let messages = chatIDs.map(o => {
             return {
                 chat_id: o,
--- tests/unit.tests/alerts.test.js
+++ tests/unit.tests/alerts.test.js
@@ -7,13 +7,18 @@ describe('Send Alerts', function() {
     it('Returns alert object', function() {
         const chatids = [93892, 1345241, 45252];
         const resourcePayload = {
-            price: 19.21,
-            name: 'Boon',
-            date: new Date()
-        }
+            price: '$73.58',
+            name: 'Unit—002',
+            date: '2020-08-31T19:00:00.000Z',
+            service: 'foundation',
+            brand: 'postdigital',
+            img: null
+        };
+
         let alertmessage = alerts.alertMessage(chatids, resourcePayload);
 
         expect(alertmessage).to.all.have.keys('chat_id', 'text');
         expect(alertmessage).to.include.something.with.property('chat_id', chatids[0]);
+        expect(alertmessage[0]).to.have.property('text', 'Unit—002 dropped on August 31, 2020.\nIt is currently trading at $73.58');
     });
 });
