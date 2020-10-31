64ae308f89d193d83faeff88484103dacf155604
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-10-31 07:54:41 +0100

    Refactor: Remove indent from following lines in '/start' response

--- components/botMessages.js
+++ components/botMessages.js
@@ -12,10 +12,9 @@ const start = function(payload) {
     else
         text += ".";
 
-    text += `
-            I'm here to alert you on products, artwork released by varying artists.
-            Right now, you can choose to subscribe to all new releases, or drops. Eventually, you will have artists you look forward to and then you can subscribe to those artists.
-            Choose 'Subscribe' to learn more about the different services you could subscribe to.`;
+    text += 'I\'m here to alert you on products, artwork released by varying artists.';
+    text += '\nRight now, you can choose to subscribe to all new releases, or drops. Eventually, you will have artists you look forward to and then you can subscribe to those artists.'
+    text += '\nChoose \'Subscribe\' to learn more about the different services you could subscribe to.';
     return {
         text,
         reply_markup: replyMarkup
