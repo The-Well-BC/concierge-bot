733572f8ffbddb25eea9f7fd7195ad91ac756201
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-11-02 06:36:10 +0100

    Refactor alert message
    Three message types depending on payload

--- components/alerts.js
+++ components/alerts.js
@@ -5,7 +5,29 @@ module.exports = {
         let platform = payload.service.charAt(0).toUpperCase() + payload.service.substring(1);
 
         let dateStr = (months.split(' '))[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-        let text = `${ payload.name } dropped on ${ dateStr }.\nIt is currently trading at ${ payload.price }\nBrand: ${ payload.brand }\n\n_via ${ platform }_`;
+
+        let text = payload.name;
+
+        if(payload.action) {
+            let action = ( payload.action.match(/sell/i) ) ?
+                'sold' : 
+                (payload.action.match(/buy/i)) ? 'bought' :
+                'redeemed';
+
+            text += ` was ${ action } on ${ dateStr }.\nIt is currently trading at ${ payload.price }`;
+        } else if(payload.status) {
+            if(payload.status.match(/close/i)) {
+                if(payload.closedOn) {
+                    date = new Date(payload.closedOn);
+                    dateStr = (months.split(' '))[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+                }
+                text += ` closed on ${ dateStr } at a price of ${ payload.price }.`;
+            } else {
+                text += ` opened on ${ dateStr } at a price of ${ payload.price }.\nIt is currently trading at ${ payload.price }.`;
+            }
+        }
+
+        text += `\nBrand: ${ payload.brand }\n\n_via: ${ platform }_`;
         let photo = (payload.img != null || undefined) ?
             payload.img : null;
 
@@ -13,6 +35,7 @@ module.exports = {
             return {
                 chat_id: o,
                 text,
+                parse_mode: 'Markdown',
                 ...(photo != null) && { photo }
             }
         });
