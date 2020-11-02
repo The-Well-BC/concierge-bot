b0ce557142204835c1801401b942f206f9899cf5
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-11-02 06:33:01 +0100

    Update foundation fetch function

--- components/resources/foundation.js
+++ components/resources/foundation.js
@@ -2,36 +2,148 @@ const axios = require('axios');
 
 module.exports = {
     fetchDrops: function() {
-        const url = 'https://api.thegraph.com/subgraphs/name/f8n/f8n-xdai';
+        let url = 'https://api.thegraph.com/subgraphs/name/f8n/f8n-xdai';
         const now = new Date();
-        let startTime = new Date().setMonth(now.getMonth() - 4);
+        let startTime = new Date().setDate(now.getDate() - 1);
         startTime = parseInt(startTime/1000);
-        console.log('Start Time', startTime);
+
+        let marketAction;
+        let nftMarketBids;
 
         const query = `{
-            xykmarkets(where: {goLiveDate_gt: ${ startTime } }) {
-                name
-                goLiveDate
-                totalBuyPrice
+            marketActions (where: { timestamp_gt: ${ startTime } }){
+                nft {
+                    id
+                    tokenId
+                    createdBy
+                    tokenURI
+                    tokenId
+                    goLiveDate
+                    marketListing {
+                        listedOn
+                    }
+                }
                 brand {
-                    name 
+                    name
+                    symbol
+                }
+                timestamp
+                network
+                transactionHash
+                actionType
+                market {
+                    name
+                    symbol
+                    totalBuyPrice
                 }
             }
-        }`;
+
+            nftmarketBids(where: { placedOn_gt: ${ startTime } }) {
+                value
+                status
+                nft {
+                    id
+                    tokenId
+                    createdBy
+                    tokenURI
+                    tokenId
+                    goLiveDate
+                    minBidPrice
+                    marketListing {
+                        listedOn
+                    }
+                    brand {
+                        name
+                        symbol
+                    }
+
+                    nft721Base {
+                        name
+                        symbol
+                    }
+                }
+            }
+        }`
 
         return axios.post(url, { query })
         .then(res => {
-            const drops = res.data.data.xykmarkets;
-            return drops.map( item => {
+
+            marketAction = res.data.data.marketActions;
+            nftMarketBids = res.data.data.nftmarketBids;
+
+            let counter = 0;
+
+            const fetchNftDetails = function() {
+                if(counter == nftMarketBids.length) 
+                    return;
+
+                let nft = nftMarketBids[counter].nft;
+                url = `https://api.foundation.app/nft/${ nft.tokenId }`;
+                return axios.get(url)
+                .then(res => {
+                    counter++;
+                    console.log('NMFT DATA', res.data);
+                    nft.name = res.data.name;
+                    nft.image = res.data.image;
+                    console.log('neft update', nft);
+                    return fetchNftDetails();
+                });
+            }
+
+            return fetchNftDetails();
+        }).then(res => {
+
+            let drops = marketAction.map( item => {
+                let type = (item.nft) ? 'nft' : 'xyk';
+                if(item.nft)
+                    throw new Error('NFT EXISTS. Sorry. NOt an error');
+
+                const showcase = item.nft || item.market;
+                if(item.market) {
+                    showcase.price = showcase.totalBuyPrice
+                } else {
+                    showcase.price = showcase.minBidNow
+                }
+
+                const brand = item.brand;
+                item.date = item.timestamp;
+
                 return {
-                    price: '$' + (item.totalBuyPrice / 1000000000000000000).toFixed(2),
-                    name: item.name,
-                    date: new Date(item.goLiveDate * 1000),
+                    price: '$' + (showcase.price / 1000000000000000000).toFixed(2),
+                    name: showcase.name,
+                    date: new Date(item.date * 1000),
                     service: 'foundation',
-                    brand: item.brand.name,
+                    brand: brand.name,
+                    action: item.actionType,
                     img: null
                 }
             });
+
+            let bidDrops = nftMarketBids.map( item => {
+                let type = 'nft';
+
+                const showcase = item.nft;
+
+                showcase.price = showcase.minBidNow
+
+                const brand = item.nft.brand;
+                item.date = item.timestamp;
+
+                return {
+                    price: '$' + (showcase.price / 1000000000000000000).toFixed(2),
+                    name: showcase.name,
+                    date: new Date(item.date * 1000),
+                    service: 'foundation',
+                    brand: brand.name,
+                    status: item.status,
+                    action: item.actionType,
+                    img: showcase.image
+                }
+            });
+
+            drops.push(...bidDrops);
+
+            return drops;
         });
     }
 }
--- tests/integration.tests/foundation.test.js
+++ tests/integration.tests/foundation.test.js
@@ -1,8 +1,9 @@
 const chai = require('chai').use(require('chai-things'));
+chai.use(require('chai-subset'));
 const expect = chai.expect;
 const foundation = require('../../components/resources/foundation');
 
-describe('Fetch drops from foundation', function() {
+describe('#dev Fetch drops from foundation', function() {
     it('Fetch drops from foundation', function() {
         return foundation.fetchDrops()
         .then(res => {
@@ -12,7 +13,12 @@ describe('Fetch drops from foundation', function() {
             expect(price0[0]).to.equal('$');
             expect(price0.length - price0.indexOf('.')).to.equal(3);
             expect(res[0]).to.have.property('service', 'foundation');
-            expect(res).to.all.have.keys('date', 'name', 'price', 'service', 'brand', 'img');
+            expect(res).to.not.containSubset([{ name: undefined, }]);
+            expect(res).to.not.containSubset([{ price: '$NAN' }]);
+            expect(res).to.not.containSubset([{ action: undefined, status: undefined }]);
+            expect(res).to.not.containSubset([{ action: null, status: null }]);
+
+            expect(res).to.all.include.keys('date', 'name', 'price', 'service', 'brand', 'img', 'action');
         });
     });
 });
