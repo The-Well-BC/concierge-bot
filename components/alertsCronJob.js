2930e60bc32177bbed8b597ff93bce15ab24b818
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-01-10 21:55:58 +0100

    Fix cron jobs

--- components/alertsCronJob.js
+++ components/alertsCronJob.js
@@ -2,17 +2,19 @@ module.exports = () => {
     const cron = require('node-cron')
     const alertsModel = require('./alerts.model');
 
+    console.log('CRON JOB', process.env.NODE_ENV);
+
     if(process.env.NODE_ENV !== 'test') {
         // Ten Minutes
         cron.schedule('*/10 * * * *', function() {
             console.log('SENDING ALERTS');
-            return alertsModel.sendAlerts(null, '10 min')
+            return alertsModel.sendAlerts('10 min')
         });
 
         // Daily
         cron.schedule('0 0 * * *', function() {
             console.log('SENDING ALERTS');
-            return alertsModel.sendAlerts(null, '1 day')
+            return alertsModel.sendAlerts('1 day')
         });
     }
 }
