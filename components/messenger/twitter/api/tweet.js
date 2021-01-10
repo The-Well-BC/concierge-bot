266206bdd29e80a7d4070ca12a83e6bc6b144c02
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-01-10 13:14:29 +0100

    Update twitter api functions

--- components/messenger/twitter/api/tweet.js
+++ components/messenger/twitter/api/tweet.js
@@ -4,8 +4,8 @@ module.exports = (payload) => {
     const client = new Twitter({
         consumer_key: process.env.TWITTER_API_KEY,
         consumer_secret: process.env.TWITTER_SECRET_KEY,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        access_token_key: process.env.TWITTER_BOT_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_BOT_ACCESS_TOKEN_SECRET,
     });
 
     const body = {
