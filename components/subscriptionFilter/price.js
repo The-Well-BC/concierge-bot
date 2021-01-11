491cbe6632d658ea9ebea60c146dea955286c193
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-01-11 05:13:35 +0100

    Refactor

--- components/subscriptionFilter/price.js
+++ components/subscriptionFilter/price.js
@@ -1,5 +1,4 @@
 const extractPriceFromCurrencyString = function(priceString) {
-    console.log('PRICE STRING', priceString);
     let number = null;
     // Remove commas
     priceString = priceString.replace(/,/g, '');
