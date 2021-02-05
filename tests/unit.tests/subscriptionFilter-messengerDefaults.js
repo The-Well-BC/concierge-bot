f12e76f5b69bbf4392a32399404a11570246c870
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-05 11:17:25 +0100

    Update tests

--- tests/unit.tests/subscriptionFilter-messengerDefaults.js
+++ tests/unit.tests/subscriptionFilter-messengerDefaults.js
@@ -6,19 +6,21 @@ describe('Test Subscription filter: Filter messengers', function() {
         let users = [ {
                 chatID: 1234, messenger: 'telegram',
             }, {
-                chatID: 5678, messenger: 'telegram',
+                chatID: 5678, messenger: 'twitter',
         }];
 
         let events = [
-            { name: 'bronn', event: 'sale', creator: { name: 'Johann Du Beck', url: 'becker' } },
+            { name: 'bronn', event: 'sale', transaction: { price: '$5231' }, creator: { name: 'Johann Du Beck', url: 'becker' } },
             { name: 'Broken Broom', event: 'bid', creator: { name: 'Broomhead', } },
-            { name: 'Chicken Boo', event: 'listing', creator: { name: 'Von PlumeFeathers' } }
+            { name: 'Stable Broom', price: '$1001', event: 'drop', creator: { name: 'Broomhead', } },
+            { name: 'Chicken Boo', transaction: { price: '$231' }, event: 'sale', creator: { name: 'Von PlumeFeathers' } }
         ];
 
         let sortedEvents = subscriptionFilter(events, users);
 
         expect(sortedEvents).to.deep.eql([
-            { payload: events[0], chatIDs: [1234,5678] }
+            { payload: events[0], chatIDs: [1234,5678] },
+            { payload: events[2], chatIDs: [1234,5678] }
         ]);
     });
 });
