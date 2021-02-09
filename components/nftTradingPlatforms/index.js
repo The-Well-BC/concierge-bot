9e3b8c7800013b980c17d6fe7e376ed8b3c150ab
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-09 19:01:34 +0100

    Cleanup

--- components/nftTradingPlatforms/index.js
+++ components/nftTradingPlatforms/index.js
@@ -4,7 +4,6 @@ const fetchCreators = require('./fetchCreators');
 module.exports = (startTime ) => {
     if(!startTime)
         throw new Error('Start time is not specified');
-    console.log('START TIME', new Date(startTime).toISOString());
     
     return {
         fetchEvents: (limit) => events(startTime, limit),
