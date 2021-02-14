7b53d966ee51a5cbf5e46284784071e2268cdb93
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-14 18:46:08 +0100

    Update default subscription filters

--- components/messenger/twitter/defaultSubscriptionFilters.js
+++ components/messenger/twitter/defaultSubscriptionFilters.js
@@ -1,5 +1,7 @@
 module.exports = [
     { events: ['drop'] },
-    { txPriceGTE: '$1000' },
+    { txPriceGTE: '$1000', platforms: ['superrare', 'foundation'] },
+    { txPriceGTE: '$500', platforms: ['zora'] },
+    { txPriceGTE: '$300', platforms: ['nifty'] },
     { priceGT: '$1000' }
 ]
