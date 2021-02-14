eaba9f2fb5693f5729d1b65bebe8d5d5a1496d0c
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-14 18:47:55 +0100

    Update: Increase tx price filter for Nifty Gateway

--- components/messenger/telegram/defaultSubscriptionFilters.js
+++ components/messenger/telegram/defaultSubscriptionFilters.js
@@ -2,6 +2,6 @@ module.exports = [
     { events: ['drop'] },
     { txPriceGTE: '$1000', platforms: ['superrare', 'foundation'] },
     { txPriceGTE: '$500', platforms: ['zora'] },
-    { txPriceGTE: '$300', platforms: ['nifty'] },
+    { txPriceGTE: '$3000', platforms: ['nifty'] },
     { priceGT: '$1000' }
 ]
