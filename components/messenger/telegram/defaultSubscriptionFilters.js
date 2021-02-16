4aa74e5df2048369c7d46c5d4d4ccb98416e3d4d
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-16 21:01:49 +0100

    Update default filter on telegram

--- components/messenger/telegram/defaultSubscriptionFilters.js
+++ components/messenger/telegram/defaultSubscriptionFilters.js
@@ -2,6 +2,7 @@ module.exports = [
     { events: ['drop'], platforms: ['superrare', 'foundation', 'zora'] },
     { txPriceGTE: '$1000', platforms: ['superrare', 'foundation'] },
     { txPriceGTE: '$300', platforms: ['zora'] },
+    { events: ['sale'], platforms: ['zora'] },
     { txPriceGTE: '$3000', platforms: ['nifty'] },
     { priceGT: '$1000' }
 ]
