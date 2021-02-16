c13bab71282a5a1a21062f4ffb23204f997248df
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-16 09:08:31 +0100

    Reduce Zora sale tx price to 00

--- components/messenger/telegram/defaultSubscriptionFilters.js
+++ components/messenger/telegram/defaultSubscriptionFilters.js
@@ -1,7 +1,7 @@
 module.exports = [
     { events: ['drop'], platforms: ['superrare', 'foundation', 'zora'] },
     { txPriceGTE: '$1000', platforms: ['superrare', 'foundation'] },
-    { txPriceGTE: '$500', platforms: ['zora'] },
+    { txPriceGTE: '$300', platforms: ['zora'] },
     { txPriceGTE: '$3000', platforms: ['nifty'] },
     { priceGT: '$1000' }
 ]
