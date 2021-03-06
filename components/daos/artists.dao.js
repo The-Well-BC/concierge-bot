179f24daae047ca51d32277b7c83e37377d6a7b9
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-03-06 17:25:38 +0100

    Add artist DAO

---
+++ components/daos/artists.dao.js
@@ -0,0 +1,10 @@
+const tables = require('../../config/tables');
+
+const db = require('postgresorm');
+
+const defaultFilterOrder = 'ORDER BY _id ASC';
+
+module.exports = {
+    fetchArtists() {
+    }
+}
