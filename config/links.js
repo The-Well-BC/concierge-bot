cb9a917b75fb1ffdb7d214694b9d16ebfda49905
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-03-12 17:57:00 +0100

    Update telegram webhook route

--- config/links.js
+++ config/links.js
@@ -1,4 +1,4 @@
 module.exports = {
-    telegramWebhook: `/${ process.env.TELEGRAM_BOT_TOKEN }`,
+    telegramWebhook: `/telegram/webhook/${ process.env.RANDOM_STRING}`,
     twitterWebhook: `/twitter/webhook/${ process.env.RANDOM_STRING }`,
 }
