39485fd04312789ca6e0a8ee4acfe11159d876f7
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-03-13 01:18:50 +0100

    Remove Nifty Gateway

--- components/nftTradingPlatforms/fetchEvents.js
+++ components/nftTradingPlatforms/fetchEvents.js
@@ -2,29 +2,27 @@ const creatorDAO = require('../daos/artists.dao');
 const foundation = require('./foundation');
 const zora = require('./zora');
 const superrare = require('./superrare');
-const nifty = require('./nifty');
 
 module.exports = (startTime, limit = 10) => {
     if(!startTime)
         throw new Error('No start time specified');
 
-    let platforms = [foundation, superrare, nifty, zora];
+    let platforms = [foundation, superrare, zora];
 
     let drops = [];
 
     return creatorDAO.fetchArtists()
     .then(res => {
-        console.log('RES', res);
-        let creatorIDs = [ res.map(i => i.platforms.foundation),
-            res.map(i => i.platforms.zora),
-            res.map(i => i.platforms.superrare),
-            res.map(i => i.platforms.nifty)
-        ]
+        console.log('ARTISTS', res);
+        let creatorIDs = res.map(i => i.walletAddress);
+
+        console.log('CREATOR IDS', creatorIDs);
+        creatorIDs = creatorIDs.filter(i => i != null && i != '')
 
         console.log('CREATORS NICELY SORTED', creatorIDs);
 
         return Promise.all( platforms.map((p, index) => {
-            return p.fetchEvents(startTime, limit, creatorIDs[index])
+            return p.fetchEvents(startTime, limit, creatorIDs)
         }))
     }).then(res => res.flat());
 }
